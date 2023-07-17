a4d0e7d8d62d3b641e1886e981e3556f8c3e2c51
Isaac Anderson <isaacaloysious@gmail.com>
2023-07-17 15:49:33 -0700

    working, bit messy but double checked results and seem accurate

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,105 +1,128 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
- 
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
-const systemMessage = "You are in a project where you'll need to improve the training for an LLM (Large Language Model). Given a user's prompt and two AI models' responses, your task is to Determine the rating of each model's answer - Completely Correct, Partially Correct, or Incorrect, using the Correctness Guide. Compare the responses of the two models to find which one has higher quality. Keep in mind the following aspects: Evaluate both responses based on code generation, code explanation, and code commenting. Consider general rules and deal breakers as defined in the guide. Use specific cases to accurately categorize tasks with distinct characteristics. Your evaluation should conclude with answering 3 questions: What are the individual ratings of the responses? After rating the responses individually, which one is better? What's your confidence level in your answers? Lastly, provide comments if necessary, to explain your responses or rating decisions. Make sure your comments answer why you chose a particular response and why one output is superior/inferior to another. Do not forget to review your task before submitting based on the Task Submission Checklist given. It includes steps like thoroughly reading the prompt, checking for grammatical errors, evaluating and comparing the responses, assessing your confidence in responding to this prompt, and writing a proper comment to elaborate on your decisions."
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+const systemMessage =
+  "You are in a project where you'll need to improve the training for an LLM (Large Language Model). Given a user's prompt and two AI models' responses, your task is to Determine the rating of each model's answer - Completely Correct, Partially Correct, or Incorrect, using the Correctness Guide. Compare the responses of the two models to find which one has higher quality. Keep in mind the following aspects: Evaluate both responses based on code generation, code explanation, and code commenting. Consider general rules and deal breakers as defined in the guide. Use specific cases to accurately categorize tasks with distinct characteristics. Your evaluation should conclude with answering 3 questions: What are the individual ratings of the responses? After rating the responses individually, which one is better? What's your confidence level in your answers? Lastly, provide comments if necessary, to explain your responses or rating decisions. Make sure your comments answer why you chose a particular response and why one output is superior/inferior to another. Do not forget to review your task before submitting based on the Task Submission Checklist given. It includes steps like thoroughly reading the prompt, checking for grammatical errors, evaluating and comparing the responses, assessing your confidence in responding to this prompt, and writing a proper comment to elaborate on your decisions.";
 
 const correctnessGuide = {
-  "header": "The following is a reference, please always use this guide.",
-  "sections": {
-    "ExplanationOfCorrectnessGuide": {
-      "Rating": "It’s the category that you have inside the tasks. For each rate we have an explanation of when it should be categorized with this rating.",
-      "Description": "This section explains this question: which represents completely correct, incorrect or partially incorrect?",
-      "CategoriesIncluded": {
-        "description": "Within this section, we encounter the primary rating categories that are utilized to classify various aspects. Each category is then further divided into distinct elements that help in assessing specific criteria.",
-        "CodeGeneration": "This category concentrates on evaluating the overall quality of the code present in each response. Factors such as code structure, readability, adherence to best practices, and successful implementation are taken into account during assessment.",
-        "CodeExplanation": "Here, the focus lies on the explanations accompanying the code. These explanations are examined based on their clarity, depth, and coherency to ensure effective communication of the purpose and functionality of the code.",
-        "CodeCommenting": "This category deals with the inclusion and quality of comments within the code. The usage of comments is evaluated in terms of providing additional context, clarifying complex sections, and improving the code's overall understanding and maintainability."
+  header: "The following is a reference, please always use this guide.",
+  sections: {
+    ExplanationOfCorrectnessGuide: {
+      Rating:
+        "It’s the category that you have inside the tasks. For each rate we have an explanation of when it should be categorized with this rating.",
+      Description:
+        "This section explains this question: which represents completely correct, incorrect or partially incorrect?",
+      CategoriesIncluded: {
+        description:
+          "Within this section, we encounter the primary rating categories that are utilized to classify various aspects. Each category is then further divided into distinct elements that help in assessing specific criteria.",
+        CodeGeneration:
+          "This category concentrates on evaluating the overall quality of the code present in each response. Factors such as code structure, readability, adherence to best practices, and successful implementation are taken into account during assessment.",
+        CodeExplanation:
+          "Here, the focus lies on the explanations accompanying the code. These explanations are examined based on their clarity, depth, and coherency to ensure effective communication of the purpose and functionality of the code.",
+        CodeCommenting:
+          "This category deals with the inclusion and quality of comments within the code. The usage of comments is evaluated in terms of providing additional context, clarifying complex sections, and improving the code's overall understanding and maintainability.",
       },
-      "GeneralRules": "The General Rules serve as the fundamental guidelines that are used to assess whether the tasks meet the essential requirements for their appropriate categorization. These rules form the basis for evaluating the tasks and ensuring their proper classification.",
-      "DealBreakers": "Deal Breakers are strict rules or criteria that are applied to determine when a task does not fulfill the essential requirements. They act as non-negotiable factors that serve as clear indicators of why a task cannot be given specific Individual Rating.",
-      "SpecificCases": "The Specific Cases serve as the specific guidelines that are used to correctly categorize tasks that possess distinct characteristics or fall under particular circumstances. These guidelines ensure that tasks with specific attributes or scenarios are correctly classified based on the provided criteria."
+      GeneralRules:
+        "The General Rules serve as the fundamental guidelines that are used to assess whether the tasks meet the essential requirements for their appropriate categorization. These rules form the basis for evaluating the tasks and ensuring their proper classification.",
+      DealBreakers:
+        "Deal Breakers are strict rules or criteria that are applied to determine when a task does not fulfill the essential requirements. They act as non-negotiable factors that serve as clear indicators of why a task cannot be given specific Individual Rating.",
+      SpecificCases:
+        "The Specific Cases serve as the specific guidelines that are used to correctly categorize tasks that possess distinct characteristics or fall under particular circumstances. These guidelines ensure that tasks with specific attributes or scenarios are correctly classified based on the provided criteria.",
     },
-    "Ratings": {
-      "Completely_correct": {
-        "Overall": "A completely correct answer is one that has no errors in the code and answers the question correctly.",
-        "CategoriesIncluded": "It is not mandatory for the answer to include both explanations and comments simultaneously. However, it should contain at least one of them, and there might be minor errors present.",
-        "GeneralRules": "If the prompt explicitly requests no explanations or comments, there should be none.",
-        "Dealbreakers": "The code generation must be correct. If there is a compilation or runtime error, the answer should be 'partially correct'. The code explanation and comments might contain minor errors, such as grammatical or verbose mistakes. If there are significant errors, the answer would be considered 'partially correct'.",
-        "SpecificCases": {
-          "CodeGeneration": [
+    Ratings: {
+      Completely_correct: {
+        Overall:
+          "A completely correct answer is one that has no errors in the code and answers the question correctly.",
+        CategoriesIncluded:
+          "It is not mandatory for the answer to include both explanations and comments simultaneously. However, it should contain at least one of them, and there might be minor errors present.",
+        GeneralRules:
+          "If the prompt explicitly requests no explanations or comments, there should be none.",
+        Dealbreakers:
+          "The code generation must be correct. If there is a compilation or runtime error, the answer should be 'partially correct'. The code explanation and comments might contain minor errors, such as grammatical or verbose mistakes. If there are significant errors, the answer would be considered 'partially correct'.",
+        SpecificCases: {
+          CodeGeneration: [
             "The code is free from any compilation and runtime errors. You don’t have to compile and run the program, please use your best judgment based on domain knowledge",
             "Correctness for functionality - the generated code solves the problem presented by the input prompts correctly i.e. performs the expected functionality in the input prompt perfectly",
             "Correctness for syntax and semantics - the code syntax and semantics are correct.",
-            "All cases contemplated when code generation is completely correct."
+            "All cases contemplated when code generation is completely correct.",
           ],
-          "CodeExplanation": [
+          CodeExplanation: [
             "The explanation correctly explains the code provided but may have a few acceptable grammatical errors or misses some small points.",
             "The explanation correctly represents the complete code provided.",
             "No explanation was provided, but we have commenting code.",
-            "All cases contemplated when code explanation is completely correct or partially correct."
+            "All cases contemplated when code explanation is completely correct or partially correct.",
           ],
-          "CodeCommenting": [
+          CodeCommenting: [
             "The comments correctly explain the code snippets being commented but may have a few acceptable grammatical errors or miss some small points.",
             "The comments are semantically and grammatically correct.",
-            "No comments were provided, but we have a code explanation."
-          ]
-        }
+            "No comments were provided, but we have a code explanation.",
+          ],
+        },
       },
-      "Partially_correct": {
-        "Overall": "A partially correct answer is one that is beneficial to me in relation to the prompt request, despite the presence of errors. If the code proves useful to me but contains some errors, such as indentation issues, it would still be considered partially correct. Explanations or comments may or may not be included in the answer, but if present, these explanations can contain errors as long as they maintain a positive tone and avoid any false information.",
-        "SpecificCases": {
-          "CodeGeneration": [
+      Partially_correct: {
+        Overall:
+          "A partially correct answer is one that is beneficial to me in relation to the prompt request, despite the presence of errors. If the code proves useful to me but contains some errors, such as indentation issues, it would still be considered partially correct. Explanations or comments may or may not be included in the answer, but if present, these explanations can contain errors as long as they maintain a positive tone and avoid any false information.",
+        SpecificCases: {
+          CodeGeneration: [
             "The code correctly addresses the use case and solves the problem but there are minor issues like missing semicolons, unused variables, etc.",
             "If the generated code contains critical errors, the answer should be considered 'incorrect'.",
             "If the code explanation or comments have an aggressive tone, are inaccurate, or contain incorrect information, the answer should also be considered 'incorrect'.",
             "If the code generation is incorrect (has critical errors) isn't partially correct.",
-            "If the code explanation or comments has a bad tone (aggressive), or are inaccurate and contain information that is not correct, it is incorrect."
+            "If the code explanation or comments has a bad tone (aggressive), or are inaccurate and contain information that is not correct, it is incorrect.",
           ],
-          "CodeExplanation": "The response does not provide any explication or comes from an explanation that is not complete.",
-          "CodeCommenting": "The response does not provide any comments or provides comments that are not complete."
-        }
+          CodeExplanation:
+            "The response does not provide any explication or comes from an explanation that is not complete.",
+          CodeCommenting:
+            "The response does not provide any comments or provides comments that are not complete.",
+        },
       },
-      "Incorrect": {
-        "SpecificCases": {
-          "CodeGeneration": [
+      Incorrect: {
+        SpecificCases: {
+          CodeGeneration: [
             "The code does not produce the desired result.",
             "The code invents things. These can range from depreciated parameters for API calls, to entire APIs themselves.",
             "The response is given in another programming language than the one requested",
-            "All cases contemplated when code generation is incorrect."
+            "All cases contemplated when code generation is incorrect.",
           ],
-          "CodeExplanation": "The explanation is inaccurate and contains information that is completely/almost completely incorrect.",
-          "CodeCommenting": "The explanation is inaccurate and contains information that is completely/almost completely incorrect."
-        }
+          CodeExplanation:
+            "The explanation is inaccurate and contains information that is completely/almost completely incorrect.",
+          CodeCommenting:
+            "The explanation is inaccurate and contains information that is completely/almost completely incorrect.",
+        },
       },
-      "Could_not_judge": "When the task is unrelated to coding, meaning that the topic of the task is not related to any aspect of coding or programming."
-    }
-  }
-}
+      Could_not_judge:
+        "When the task is unrelated to coding, meaning that the topic of the task is not related to any aspect of coding or programming.",
+    },
+  },
+};
 
 // IMPORTANT! Set the runtime to edge
-export const runtime = 'edge'
- 
+export const runtime = "edge";
+
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json()
+  const { messages } = await req.json();
   messages.unshift({
-    role: 'system',
-    content: systemMessage + "\n\nCorrectness Guide:\n" + JSON.stringify(correctnessGuide, null, 2),
+    role: "system",
+    content:
+      systemMessage +
+      "\n\nCorrectness Guide:\n" +
+      JSON.stringify(correctnessGuide, null, 2),
   });
- 
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.createChatCompletion({
-    model: 'gpt-4-0613',
+    model: "gpt-4-0613",
     stream: true,
-    messages
-  })
+    messages,
+  });
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
+  const stream = OpenAIStream(response);
   // Respond with the stream
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
